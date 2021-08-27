cd3a0dba405dfe5a30041aecaf6b30163c8f6095
Juho Salli <juho.salli@gmail.com>
2021-08-27 13:28:57 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from "./logo.svg";
+import TextLoop from "react-text-loop";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>
+          <span>Jööti on </span>
+          <TextLoop>
+            <code>tutkimusrekisteri</code>
+            <code>syysmyrsky</code>
+            <code>julkaisujärjestelmä</code>
+            <code>pötkylä</code>
+            <code>eläin</code>
+            <code>ajatushautomo</code>
+            <code>kevätmyrsky</code>
+          </TextLoop>
+          .
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
